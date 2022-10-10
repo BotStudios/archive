124152d4c3245da9f4898b8af01f75ca1ebd1775
iamsurge <112551585+iamsurge@users.noreply.github.com>
2022-10-10 13:40:23 +0800

    Update maintain.js

--- maintain.js
+++ maintain.js
@@ -5,7 +5,7 @@ async function browser(url) {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
 
-    await page.goto(url);
+    await page.goto(url, {waitUntil: 'networkidle0', timeout: 60000, networkIdleTimeout: 1000 * 3});
     await page.waitFor(80000);
     writeFileSync("./output.txt", await page.content());
     await page.screenshot({path: './screenshot.png', fullPage: true});
