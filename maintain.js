8f2c2cfd4434d62cfcdce0f150ed2e9308a3a945
iamsurge <112551585+iamsurge@users.noreply.github.com>
2022-10-10 13:33:36 +0800

    Update maintain.js

--- maintain.js
+++ maintain.js
@@ -2,13 +2,13 @@ const puppeteer = require('puppeteer');
 const { readFileSync, writeFileSync } = require("fs")
 
 async function browser(url) {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-    await page.goto(url)
+    await page.goto(url);
 
-    await page.screenshot({path: './screenshot.png'});
     writeFileSync("./output.txt", await page.content());
+    await page.screenshot({path: './screenshot.png'});
     await browser.close();
 }
 
