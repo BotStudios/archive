90f740a12b0dcae2a0ca98e257d0f22754ba3004
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-10-15 23:16:28 +0800

    Update maintain.js

--- maintain.js
+++ maintain.js
@@ -5,12 +5,12 @@ async function browser(url) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    await page.goto(`https://www.google.com/search?q=site:${url}`, {waitUntil: 'networkidle0'});
+    await page.goto(`https://www.google.com/search?q=${url}`, {waitUntil: 'networkidle0'});
     await page.evaluate(`([...document.querySelectorAll("a")].find(a => a.href === '${url}'))?.click()`)
- //   await page.waitFor(10000);
+    await page.waitFor(5000);
     writeFileSync("./output.txt", await page.content());
     await page.screenshot({path: './screenshot.png', fullPage: true});
     await browser.close();
 }
 
-browser("https://joe.js.org/");
+browser("https://joelee.works/");
