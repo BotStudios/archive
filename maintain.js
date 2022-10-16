f256cf08c7d318f66817e21fd34a0ef0cb8c84de
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-10-16 20:39:06 +0800

    Update maintain.js

--- maintain.js
+++ maintain.js
@@ -8,13 +8,14 @@ async function browser(url) {
     await page.goto(`https://www.google.com/search?q=${url}`, {waitUntil: 'networkidle0'});
     await page.evaluate(`([...document.querySelectorAll("a")].find(a => a.href === 'https://github.com/${url}'))?.click()`)
     await page.waitFor(5000);
+    await page.screenshot({path: './screenshot.png', fullPage: true});
     const page2 = await browser.newPage();
 
     await page2.goto(`https://www.google.com/search?q=leecheeyong`, {waitUntil: 'networkidle0'});
     await page2.evaluate(`([...document.querySelectorAll("a")].find(a => a.href === 'https://github.com/leecheeyong'))?.click()`)
     await page2.waitFor(5000);
+    await page2.screenshot({path: './screenshot1.png', fullPage: true});
     writeFileSync("./output.txt", await page.content());
-    await page2.screenshot({path: './screenshot.png', fullPage: true});
     await browser.close();
 }
 
