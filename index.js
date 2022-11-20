3d2de7bfa22636093efb4c4dd03ac81c433566b9
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-11-20 14:49:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Made with love by Chee Yong Lee (https://joe.js.org) https://github.com/leecheeyong
 // This project is available as an open source under the MIT License
-const customSearch = ["discord"];
+const customSearch = ["discord", "webdevsimplified", "web", "wwdc"];
 const maxDuration = 5;
 const getSize = require('get-folder-size');
 const { performance } = require('perf_hooks');
